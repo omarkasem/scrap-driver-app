f2a97f587d6161dab3b2248f3910e646c13be473
omarkasem <omar.kasem207@gmail.com>
2025-01-15 19:02:28 +0200

    driver

--- frontend/templates/view-collections.php
+++ frontend/templates/view-collections.php
@@ -78,7 +78,7 @@ $collections = new WP_Query($args);
             <thead>
                 <tr>
                     <th><?php _e('ID', 'scrap-driver'); ?></th>
-                    <th><?php _e('Customer', 'scrap-driver'); ?></th>
+                    <th><?php _e('Driver', 'scrap-driver'); ?></th>
                     <th><?php _e('Vehicle', 'scrap-driver'); ?></th>
                     <th><?php _e('Collection Date', 'scrap-driver'); ?></th>
                     <th><?php _e('Status', 'scrap-driver'); ?></th>
@@ -90,7 +90,8 @@ $collections = new WP_Query($args);
                 if ($collections->have_posts()) :
                     while ($collections->have_posts()) : $collections->the_post();
                         $collection_id = get_the_ID();
-                        $customer_name = get_field('customer_name');
+                        $driver_id = get_field('assigned_driver');
+                        $driver_name = $driver_id ? get_user_by('id', $driver_id)->display_name : __('Unassigned', 'scrap-driver');
                         $vehicle_make = get_field('vehicle_info_make');
                         $vehicle_model = get_field('vehicle_info_model');
                         $vehicle_plate = get_field('vehicle_info_plate');
@@ -99,7 +100,7 @@ $collections = new WP_Query($args);
                         ?>
                         <tr>
                             <td><?php echo esc_html($collection_id); ?></td>
-                            <td><?php echo esc_html($customer_name); ?></td>
+                            <td><?php echo esc_html($driver_name); ?></td>
                             <td>
                                 <?php 
                                 echo esc_html(sprintf(
